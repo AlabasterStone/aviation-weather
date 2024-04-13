c1e8b14d3ec2fbc1afbe5851adb2518359977b04
tourkveg <thewavylines@outlook.com>
2024-04-13 13:36:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    METERs_data = requests.get("https://aviationweather.gov/cgi-bin/data/metar.php?ids=ZBAA")
-    TAFs_data = requests.get("https://aviationweather.gov/cgi-bin/data/taf.php?ids=ZBAA")
+@app.get("/{searchID}")
+def read_root(searchID):
+    METERs_data = requests.get(f"https://aviationweather.gov/cgi-bin/data/metar.php?ids={searchID}")
+    TAFs_data = requests.get(f"https://aviationweather.gov/cgi-bin/data/taf.php?ids={searchID}")
     return METERs_data.content+TAFs_data.content
