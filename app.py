1dfc7052dddcae8ed1fc66e3af822fbbe39855ca
tourkveg <thewavylines@outlook.com>
2024-04-13 13:49:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+origins = [
+    "github.io",
+]
+
+# 后台api允许跨域
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/get_data")
 def read_root(searchID):
     METERs_data = requests.get(f"https://aviationweather.gov/cgi-bin/data/metar.php?ids={searchID}")
