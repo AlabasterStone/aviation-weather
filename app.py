4cb87b9999db9f8accaebf4266e1e431d88b47a4
tourkveg <thewavylines@outlook.com>
2024-04-13 14:37:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "github.io",
-    "stblx.github.io",
-]
 
 # 后台api允许跨域
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
